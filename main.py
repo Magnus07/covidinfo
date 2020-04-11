db275b7691b7c50ac99c272aaa072e5eecf1cd9e
1TrueNova1 <kuch19.05.02@gmail.com>
2020-04-11 16:30:55 +0300

    final release

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from config import *
 
-
 def get_stat_by_country(country):
     response = apiRequest(covid_country_by_name, headers = get_headers("coronavirus-monitor.p.rapidapi.com"),params = {"country" : country})
     jsoned = json.loads(response)
@@ -15,6 +14,7 @@ def start_message(message):
     keyboard_markup = telebot.types.ReplyKeyboardMarkup(True, False)
     keyboard_markup.row('Пошук за назвою країни', 'Пошук за містом')
     keyboard_markup.row('Пошук по координатам', 'Додати інформацію про випадок зараження')
+    keyboard_markup.row('Статистика по всьому світу')
     bot.send_message(message.chat.id, 'Яким чином ви бажаєте взаємодіяти з ботом?', reply_markup=keyboard_markup)
 
 #writes user's input to mongoDB
@@ -26,7 +26,6 @@ def save_adress(message):
 	    bot.send_message(message.from_user.id, "Інформація збережена")
 	else:
 		bot.send_message(message.chat.id, "Ви ввели некоректні дані")
-
 
 def enter_adress(message):
 	if (message.text!=None):
@@ -59,6 +58,14 @@ def get_info_by_location(message):
 	elif(message.text=="Додати інформацію про випадок зараження"):
 		bot.send_message(message.chat.id, "Уведіть ім'я та прізвище: ")
 		bot.register_next_step_handler(message, enter_city)
+	elif(message.text=="Статистика по всьому світу"):
+		country_cases = nameApiRequest(country_by_name_url, country_by_name_headers)
+		jsoned = json.loads(country_cases.text)
+		output = "Країна\nІнфіковано\n"
+		print(jsoned)
+		for x in range(1,100):
+			output+=jsoned['countries_stat'][x]['country_name']+"\n"+jsoned['countries_stat'][x]['cases']+" "+"\n"
+		bot.send_message(message.chat.id, output)
 
 
 #sends statisics to user by coordinates
@@ -71,19 +78,6 @@ def by_coordinates(message):
 	    bot.send_message(message.chat.id, get_stat_by_country(jsoned[0]["Country"]))
 	else:
 		bot.send_message(message.chat.id, "Ви ввели некоректні дані")
-
-def by_country_name(message):
-	country_cases = nameApiRequest(country_by_name_url, country_by_name_headers)
-	jsoned = json.loads(country_cases.text)
-	found=0
-	for x in jsoned['countries_stat']:
-		if (x['country_name']==message.text):
-			bot.send_message(message.chat.id, "В країні "+message.text+
-			"\nІнфікованих: "+x['cases']+"\nЗагинуло: "+x['deaths']+"\nОдужало:"+x['total_recovered']+
-			"\nВ тяжкому стані: "+x['serious_critical']+"\n\nНові випадки:\nІнфікованих: "+x['new_cases']+"\nЗагинуло: "+x['new_deaths'])
-			found=1
-	if(found==0):
-		bot.send_message(message.chat.id, "На жаль, не має інформації про країну "+str(message.text))
 
 #sends statisics to user by country name
 def by_country_name(message):
@@ -101,7 +95,6 @@ def by_country_name(message):
 			bot.send_message(message.chat.id, "На жаль, не має інформації про країну "+message.text)
 	else:
 		bot.send_message(message.chat.id, "Ви ввели некоректні дані")
-
 
 #sends statisics to user by city name
 def by_city_name(message):
