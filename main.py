f31c3dae6b63ef776ec1f99b283d755bf2510e95
Magnus07 <stanislav.pinchuk.02@gmail.com>
2020-04-11 14:54:44 +0300

    Change structure

--- main.py
+++ main.py
@@ -1,54 +1,4 @@
-import telebot
-import pymongo
-from telebot import types
-import requests
-import os
-import json
-
-
-client     = pymongo.MongoClient(os.environ.get("MONGODB"))
-db         = client.test_database
-collection = db.test_collection
-
-user = {
-    "name" : "",
-    "adress" : ""
-}
-
-covid_url = "https://coronavirus-monitor.p.rapidapi.com/coronavirus/random_masks_usage_instructions.php"
-covid_country_by_name = "https://coronavirus-monitor.p.rapidapi.com/coronavirus/latest_stat_by_country.php"
-geo_url   = "https://geocodeapi.p.rapidapi.com/GetNearestCities"
-
-locationstring = {"latitude":"","longitude":"","range":"0"}
-
-querystring = {"latitude":"","longitude":"","range":"0"}
-
-headers = {
-    'x-rapidapi-host': "geocodeapi.p.rapidapi.com",
-    'x-rapidapi-key': "e0148a3467msh96216911de4311bp11d63djsn470777193a84"
-}
-
-bot = telebot.TeleBot(os.environ.get("API"))
-
-def get_headers(host):
-    headers["x-rapidapi-host"]=host
-    return headers
-
-country_by_name_url = "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php"
-country_by_name_headers = {
-    'x-rapidapi-host': "coronavirus-monitor.p.rapidapi.com",
-    'x-rapidapi-key': "e0148a3467msh96216911de4311bp11d63djsn470777193a84"
-}
-
-def nameApiRequest(url, headers):
-    return requests.request("GET", url, headers=headers)
-
-def apiRequest(url, headers, params):
-    return requests.request("GET", url, headers=headers, params=params).text
-
-
-def make_string(caption,data):
-    return caption + " : " + data + "\n"
+from config import *
 
 
 def get_stat_by_country(country):
@@ -58,26 +8,30 @@ def get_stat_by_country(country):
     return message
 
 
-bot = telebot.TeleBot(os.environ.get("API"))
-
 @bot.message_handler(commands=['start'])
 def start_message(message):
-        keyboard_markup = telebot.types.ReplyKeyboardMarkup(True, False)
-        keyboard_markup.row('Пошук за назвою країни', 'Пошук за назвою міста')
-        keyboard_markup.row('Пошук по координатам', 'Додати інформацію про випадок зараження')
-        bot.send_message(message.chat.id, 'Яким чином ви бажаєте взаємодіяти з ботом?', reply_markup=keyboard_markup)
+    keyboard_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    keyboard_markup.row('Пошук за назвою країни', 'Пошук за містом')
+    keyboard_markup.row('Пошук по координатам', 'Додати інформацію про випадок зараження')
+    bot.send_message(message.chat.id, 'Яким чином ви бажаєте взаємодіяти з ботом?', reply_markup=keyboard_markup)
 
 
 def save_adress(message):
-    users = db.users
     user["adress"] = message.text
     users.insert_one(user)
     bot.send_message(message.from_user.id, "Інформація збережена")
 
+
 def enter_adress(message):
     msg = bot.send_message(message.from_user.id, "Укажіть адресу")
-    user["name"] = message.text
+    user["city"] = message.text
     bot.register_next_step_handler(msg,save_adress)
+
+
+def enter_city(message):
+    msg = bot.send_message(message.from_user.id, "Укажіть місто")
+    user["name"] = message.text
+    bot.register_next_step_handler(msg,enter_adress)
 
 
 @bot.message_handler(content_types=['text'])
@@ -85,15 +39,16 @@ def get_info_by_location(message):
 	if(message.text=="Пошук за назвою країни"):
 		bot.send_message(message.chat.id, "Введіть назву країни")
 		bot.register_next_step_handler(message, by_country_name)
-	elif(message.text=="Пошук за назвою міста"):
-		bot.send_message(message.chat.id, "Введіть назву міста")
+	elif(message.text=="Пошук за містом"):
+		bot.send_message(message.chat.id, "Введіть місто")
 		bot.register_next_step_handler(message, by_city_name)
 	elif(message.text=="Пошук по координатам"):
 		bot.send_message(message.chat.id, "Відправте свої координати")
 		bot.register_next_step_handler(message, by_coordinates)
 	elif(message.text=="Додати інформацію про випадок зараження"):
-		bot.send_message(message.chat.id, "Уведіть своє ім'я та прізвище: ")
-		bot.register_next_step_handler(message, enter_adress)
+		bot.send_message(message.chat.id, "Уведіть ім'я та прізвище: ")
+		bot.register_next_step_handler(message, enter_city)
+
 
 def by_coordinates(message):
     locationstring["latitude"] = message.location.latitude
@@ -102,6 +57,7 @@ def by_coordinates(message):
     jsoned = json.loads(response)
     bot.send_message(message.chat.id, get_stat_by_country(jsoned[0]["Country"]))
 
+
 def by_country_name(message):
 	country_cases = nameApiRequest(country_by_name_url, country_by_name_headers)
 	jsoned = json.loads(country_cases.text)
@@ -109,14 +65,20 @@ def by_country_name(message):
 	for x in jsoned['countries_stat']:
 		if (x['country_name']==message.text):
 			bot.send_message(message.chat.id, "В країні "+message.text+
-			"\nІнфікованих: "+x['cases']+"\nЗагинуло: "+x['deaths']+"\nОдужало"+x['total_recovered']+
+			"\nІнфікованих: "+x['cases']+"\nЗагинуло: "+x['deaths']+"\nОдужало:"+x['total_recovered']+
 			"\nВ тяжкому стані: "+x['serious_critical']+"\n\nНові випадки:\nІнфікованих: "+x['new_cases']+"\nЗагинуло: "+x['new_deaths'])
 			found=1
 	if(found==0):
-		bot.send_message(message.chat.id, "На жаль, не має інформації про країну "+message.text)
+		bot.send_message(message.chat.id, "На жаль, не має інформації про країну "+str(message.text))
+
 
 def by_city_name(message):
-	bot.send_message(message.chat.id, "...")
-	#просто затычка для поиска по городу
-    
+    result = ""
+    for user in users.find({"city": message.text}):
+       result += user["name"] + ",м." + user["city"] + "," + user["adress"] + "\n"
+    if result == "":
+        bot.send_message(message.chat.id, "Дані про випадки захворювання в місті відсутні.")
+    else:
+	    bot.send_message(message.chat.id, "Ідентифіковані захворюванні: \n" + result)
+
 bot.polling()
