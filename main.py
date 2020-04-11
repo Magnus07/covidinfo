b4d9895d50632af759a7159bd821b71ee05ec524
1TrueNova1 <kuch19.05.02@gmail.com>
2020-04-11 13:29:21 +0300

    added logic, templates for all choices, translated

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import telebot
+from telebot import types
 import requests
 import os
 import json
-
 
 covid_url = "https://coronavirus-monitor.p.rapidapi.com/coronavirus/random_masks_usage_instructions.php"
 geo_url   = "https://geocodeapi.p.rapidapi.com/GetNearestCities"
@@ -27,31 +27,52 @@ def apiRequest(url, headers, params):
 bot = telebot.TeleBot(os.environ.get("API"))
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Получать статистику по названию или координатам?')
-		
+    keyboard_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    keyboard_markup.row('Пошук за назвою країни', 'Пошук за назвою міста')
+    keyboard_markup.row('Пошук по координатам', 'Додати інформацію про випадок зараження')
+    bot.send_message(message.chat.id, 'Яким чином ви бажаєте взаємодіяти з ботом?', reply_markup=keyboard_markup)
+
 @bot.message_handler(content_types=['text'])
 def get_info_by_location(message):
+	if(message.text=="Пошук за назвою країни"):
+		bot.send_message(message.chat.id, "Введіть назву країни")
+		bot.register_next_step_handler(message, by_country_name)
+	elif(message.text=="Пошук за назвою міста"):
+		bot.send_message(message.chat.id, "Введіть назву міста")
+		bot.register_next_step_handler(message, by_city_name)
+	elif(message.text=="Пошук по координатам"):
+		bot.send_message(message.chat.id, "Відправте свої координати")
+		bot.register_next_step_handler(message, by_coordinates)
+	elif(message.text=="Додати інформацію про випадок зараження"):
+		bot.send_message(message.chat.id, "Дайте детальний опис ситуації")
+		bot.register_next_step_handler(message, add_covid_case)
+
+def by_coordinates(message):
+	querystring["latitude"] = message.location.latitude
+	querystring["longitude"] = message.location.longitude
+	response = apiRequest(geo_url,headers,querystring).text
+	jsoned = json.loads(response)
+	bot.send_message(message.chat.id, jsoned[0]["Country"])
+	#я говорил тебе об этой функции, обнови
+
+def by_country_name(message):
 	country_cases = nameApiRequest(country_by_name_url, country_by_name_headers)
 	jsoned = json.loads(country_cases.text)
 	found=0
 	for x in jsoned['countries_stat']:
 		if (x['country_name']==message.text):
-			bot.send_message(message.chat.id, "В стране "+message.text+
-			"\nЗаражено: "+x['cases']+"\nПогибло: "+x['deaths']+"\nВыздоровело: "+x['total_recovered']+
-			"\nВ тяжелом состоянии: "+x['serious_critical']+"\n\nНовые случаи:\nЗаражено: "+x['new_cases']+"\nПогибло: "+x['new_deaths'])
+			bot.send_message(message.chat.id, "В країні "+message.text+
+			"\nІнфікованих: "+x['cases']+"\nЗагинуло: "+x['deaths']+"\nОдужало"+x['total_recovered']+
+			"\nВ тяжкому стані: "+x['serious_critical']+"\n\nНові випадки:\nІнфікованих: "+x['new_cases']+"\nЗагинуло: "+x['new_deaths'])
 			found=1
 	if(found==0):
-		bot.send_message(message.chat.id, "К сожалению, не найдено информации о стране "+message.text)
+		bot.send_message(message.chat.id, "На жаль, не має інформації про країну "+message.text)
 
-    
-@bot.message_handler(content_types=['location'])
-def get_info_by_location(message):
-    querystring["latitude"] = message.location.latitude
-    querystring["longitude"] = message.location.longitude
-    response = apiRequest(geo_url,headers,querystring).text
-    jsoned = json.loads(response)
+def by_city_name(message):
+	bot.send_message(message.chat.id, "...")
+	#просто затычка для поиска по городу
 
-    bot.send_message(message.chat.id, jsoned[0]["Country"])
-
-
+def add_covid_case(message):
+	bot.send_message(message.chat.id, "...")
+	#просто затычка для добавления нового случая
 bot.polling()
